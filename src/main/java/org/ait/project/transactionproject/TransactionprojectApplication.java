75cb189faaab2a442fbebd318cb4b91480ef6584
stephanie-ntep <ntep.step@gmail.com>
2022-08-11 18:52:00 +0700

    core(main): add generate file spring

---
+++ src/main/java/org/ait/project/transactionproject/TransactionprojectApplication.java
@@ -0,0 +1,13 @@
+package org.ait.project.transactionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ait/project/transactionproject/TransactionprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ait.project.transactionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
